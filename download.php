7098dd3dc444226192a4958bfb43b3d7d1deb0d9
Rishabh Arora <rishabharora46@gmail.com>
2016-12-04 02:11:17 +0530

    Added Youtube thumbnail.

--- download.php
+++ download.php
@@ -3,6 +3,9 @@
 $id = $_GET['id'];
 ?>
 
+<!--Youtube Video Thubmnail-->
+<img src="https://img.youtube.com/vi/<?php echo $id;?>/default.jpg"><br><br>
+
 <!--Using convert2mp3.cc javascript to generate download Link-->
 <a class="c2m3" href="javascript:convert2mp3('<?php echo $id;?>')">Download</a>
 
