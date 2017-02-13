a16da700fd030785321c5a72a9d6bde7601990de
Rishabh Arora <rishabharora46@gmail.com>
2017-02-14 02:25:42 +0530

    added album search and track search separately.

--- mp3.php
+++ mp3.php
@@ -1,25 +1,29 @@
 <?php
-    $search = "Pink Floyd";   //Search Term.
+    $q = $_GET['q'];          //Search Term.
 	
-	$videos = '';             //For Storing All links. 
+	$tracks = '';             //For Storing tracks. 
 	
-	$q1 = $search;            //Just Like that.
+	$albums = '';             //For Storing albums. 
 	
-	//Data From Last.fm
-	$q2 = urlencode($q1);   
-	$json = file_get_contents("http://ws.audioscrobbler.com/2.0/?method=track.search&track={$q2}&api_key=YOUR_API_KEY_FROM_LAST.FM&format=json");
-    $data = json_decode($json,TRUE);
+	$q2 = urlencode($q); 
+	//Tracks Data From Last.fm  
+	$json1 = file_get_contents("http://ws.audioscrobbler.com/2.0/?method=track.search&track={$q2}&api_key=a9b50f586805b069a23f929fb0399e3a&format=json&limit=5");
+    $data1 = json_decode($json1,TRUE);
 	
-	//Looping JSON data to get all tracks name from Last.fm
-    foreach ($data['results']['trackmatches']['track'] as $song)
-	    {
-	      $q3 = "{$song['artist']} {$song['name']}"; //Search term for Youtube.
+	//Albums Data From Last.fm  
+	$json2 = file_get_contents("http://ws.audioscrobbler.com/2.0/?method=album.search&album={$q2}&api_key=a9b50f586805b069a23f929fb0399e3a&format=json&limit=5");
+    $data2 = json_decode($json2,TRUE);
+	
+	
+	//Looping Tracks JSON data to get all tracks name from Last.fm
+    foreach ($data1['results']['trackmatches']['track'] as $song)
+	    {	      $q3 = "{$song['artist']} {$song['name']}"; //Search term for Youtube.
     
 	       //Including Google api with youtube service.
           require_once ('google/Google_Client.php');
           require_once ('google/contrib/Google_YouTubeService.php');
  
-          $DEVELOPER_KEY = 'API_KEY_FROM_GOOGLE_CONSOLE_WITH_YOUTUBE_ENABLED';
+          $DEVELOPER_KEY = 'AIzaSyAO6e32mq3LQaOY6T323x_AFUoSEwHn2hA';
 
           $client = new Google_Client();
           $client->setDeveloperKey($DEVELOPER_KEY);
@@ -56,15 +60,18 @@
                   }
 				  
 				  //Final Links.Download Link includes the youtube id which will be  forwarded to download page.
-   $videos .= sprintf('<p>%s &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; (%s)</p>', $searchResult['snippet']['title'],"<a href=download.php?id=".$searchResult['id']['videoId'].">Download</a>");
+   $tracks .= sprintf('<p>%s &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; (%s)</p>', $searchResult['snippet']['title'],"<a href=download.php?id=".$searchResult['id']['videoId'].">Download</a>");
 
   
-     } //Last.fm JSON data loop ends.
+     } //Last.fm loop ends.
+	 foreach ($data2['results']['albummatches']['album'] as $album)
+	 {
+		 $album_q = urlencode($album['name']);
+		 $album_a = urlencode($album['artist']);
+		 $albums .= "<p><a href=album_info.php?album=".$album_q."&artist=".$album_a.">".$album['name']." by ".$album['artist']."</p>";
+	 }
 
-echo $videos;
+echo "<h2>Tracks</h2>".$tracks."<a href=tracks.php?q=".$q.">Load More</a><br>";
+echo "<h2>Albums</h2>".$albums."<a href=albums.php?q=".$q.">Load More</a><br>";
+
 ?>
-	 
-	 
-	
-
-
