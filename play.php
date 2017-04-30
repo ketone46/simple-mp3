81c796ad28b5071f96cd1fea2b008b6bf10412d5
Rishabh Arora <rishabharora46@gmail.com>
2017-05-01 02:02:13 +0530

    play preview

--- play.php
+++ play.php
@@ -3,14 +3,14 @@
   
 	$q1 = $search;            //Just Like that.
 	
-	//Data From Last.fm
+	//Data From spotify
 	$q2 = urlencode($q1);   
 	$json = file_get_contents("https://api.spotify.com/v1/search?q={$q2}&type=track&limit=1");
     $data = json_decode($json,TRUE);
 ?>	
-	<img src="<?php echo $data['tracks']['items']['0']['album']['images']['1']['url'];?>">
+	<img src="<?php echo $data['tracks']['items']['0']['album']['images']['1']['url'];?>"> 
 	<br>
 	<audio controls>
-  <source src="<?php echo $data['tracks']['items']['0']['preview_url'] ;?>" type="audio/mpeg">
+  <source src="<?php echo $data['tracks']['items']['0']['preview_url'] ;?>" type="audio/mpeg"> 
 </audio>
 	
