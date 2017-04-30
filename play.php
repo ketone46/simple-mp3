5e722556e5c7254ce8eff43cc08e5015928653ab
Rishabh Arora <rishabharora46@gmail.com>
2017-05-01 01:48:39 +0530

    Add files via upload

---
+++ play.php
@@ -0,0 +1,16 @@
+<?php
+    $search = $_GET['id'];  
+  
+	$q1 = $search;            //Just Like that.
+	
+	//Data From Last.fm
+	$q2 = urlencode($q1);   
+	$json = file_get_contents("https://api.spotify.com/v1/search?q={$q2}&type=track&limit=1");
+    $data = json_decode($json,TRUE);
+?>	
+	<img src="<?php echo $data['tracks']['items']['0']['album']['images']['1']['url'];?>">
+	<br>
+	<audio controls>
+  <source src="<?php echo $data['tracks']['items']['0']['preview_url'] ;?>" type="audio/mpeg">
+</audio>
+	
